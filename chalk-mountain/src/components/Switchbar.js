05ff81700b05cff4519ad6b28131f28530365f3d
ZylerPierceNiece <102488020+ZylerPierceNiece@users.noreply.github.com>
2022-12-06 15:36:49 -0600

    Front end functionality

--- chalk-mountain/src/components/Switchbar.js
+++ chalk-mountain/src/components/Switchbar.js
@@ -3,19 +3,121 @@ import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Form from 'react-bootstrap/Form';
 
 export default function Switchbar(){
+    function searchFunction() {
+        var input, filter, table, tr, td, i;
+        input = document.getElementById("myInput");
+        filter = input.value;
+        filter = filter.toUpperCase();
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+        for(i=0; i < tr.length; i++) {
+          var tds = tr[i].getElementsByTagName("td");
+          var flag = false;
+          for(var j=0; j < tds.length; j++){
+            var td = tds[j];
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1){
+              flag = true;
+            }
+          }
+          if(flag) {
+              tr[i].style.display = "";
+              flag = false;
+          }else {
+            if(tr[i].id != 'tableHeader'){
+                tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+      function filterAllFunction(){
+        var filter, table, tr, td, i;
+        filter = "SERVICE";
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+        for(i=0; i < tr.length; i++) {
+          var tds = tr[i].getElementsByTagName("td");
+          var flag = false;
+          for(var j=0; j < tds.length; j++){
+            var td = tds[j];
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1){
+              flag = true;
+            }
+          }
+          if(flag) {
+              tr[i].style.display = "";
+              flag = false;
+          }else {
+            if(tr[i].id != 'tableHeader'){
+                tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+      function filterInFunction(){
+        var filter, table, tr, td, i;
+        filter = "IN-SERVICE";
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+        for(i=0; i < tr.length; i++) {
+          var tds = tr[i].getElementsByTagName("td");
+          var flag = false;
+          for(var j=0; j < tds.length; j++){
+            var td = tds[j];
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1){
+              flag = true;
+            }
+          }
+          if(flag) {
+              tr[i].style.display = "";
+              flag = false;
+          }else {
+            if(tr[i].id != 'tableHeader'){
+                tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+      function filterOutFunction(){
+        var filter, table, tr, td, i;
+        filter = "OUT-OF-SERVICE";
+        table = document.getElementById("myTable");
+        tr = table.getElementsByTagName("tr");
+      
+        for(i=0; i < tr.length; i++) {
+          var tds = tr[i].getElementsByTagName("td");
+          var flag = false;
+          for(var j=0; j < tds.length; j++){
+            var td = tds[j];
+            if (td.innerHTML.toUpperCase().indexOf(filter) > -1){
+              flag = true;
+            }
+          }
+          if(flag) {
+              tr[i].style.display = "";
+              flag = false;
+          }else {
+            if(tr[i].id != 'tableHeader'){
+                tr[i].style.display = "none";
+            }
+          }
+        }
+      }
+
     return(
         <div className="switchbar">
              <ButtonGroup className="switch" aria-label="First group">
-                <Button variant="light">All</Button>
-                <Button>In-Service</Button>
-                <Button>Out-of-Service</Button>
+                <Button variant="light" onClick={filterAllFunction}>All</Button>
+                <Button onClick={filterInFunction}>In-Service</Button>
+                <Button onClick={filterOutFunction}>Out-of-Service</Button>
             </ButtonGroup>
             <ButtonGroup className="search" aria-label="Second group">
             <Form className="d-flex">
-                <Form.Control type="search" placeholder="Search" aria-label="Search"/>
+                <Form.Control type="search" id="myInput" onKeyUp={searchFunction} placeholder="Search for Assets" aria-label="Search"/>
             </Form>
             </ButtonGroup>
         </div>
-        
     )
 }
--- chalk-mountain/src/pages/Trailers.js
+++ chalk-mountain/src/pages/Trailers.js
@@ -1,13 +1,45 @@
 import Navbar from "../components/Navbar"
 import Switchbar from "../components/Switchbar"
-import Table from "../components/Table"
+import { AssetTable } from "../components/Table"
+import Popup from '../pages/Popup';
+import { useState } from 'react';
+
 export default function Trailers(){
+    const [isOpen, setIsOpen] = useState(false);
+
+    const togglePopup = () => {
+      setIsOpen(!isOpen);
+    }
     return (
     <div>
     <Navbar/>
     <h1>Trailers Only Table</h1>
     <Switchbar/>
-    <Table/>
+    <div className="tableSection">
+    <AssetTable
+        assets={[
+          { id: 1,
+            type: 'Trailer',
+            asset: 'ABC',
+            status: 'IN-SERVICE',
+            date: '02/12/2022',
+            employee: 'J.F.',
+            notes: '', 
+            modify: <input type="button" value="update" onClick={togglePopup}
+          /> },
+          { id: 4, 
+            type: 'Trailer',
+            asset: 'JKL', 
+            status: 'IN-SERVICE', 
+            date: '06/28/2022', 
+            employee: 'Z.N.', 
+            notes: '', 
+            modify: <input type="button" value="update" onClick={togglePopup}
+          />},
+        ]}
+      />
+       {isOpen && <Popup handleClose={togglePopup}/>}
+       </div>
     </div>
     )
 }
