33f5b51a94f2a13eeee1a06471b6e86bad3a8c6a
Matthew Bolding <matthew.bolding@tcu.edu>
2023-01-18 08:59:39 -0600

    Backend refactoring

--- db-utils/operations.js
+++ db-utils/operations.js
@@ -2,10 +2,6 @@ const { user } = require('./config-env');
 const config = require('./config-env'),
       sql    = require('mssql');
 
-/**
- * 
- * @returns all assets from the equipment table
- */
 const viewAll = async() => {
     try {
         let pool = await sql.connect(config);
@@ -21,8 +17,7 @@ const viewAll = async() => {
 const viewAsset = async(UNITNUMBER) => {
     try {
         let pool = await sql.connect(config);
-        let assets = pool.request().query("execute procViewAsset @unitnumber = '" + UNITNUMBER + "'")
-        console.log(assets);
+        let assets = pool.request().query("EXECUTE procViewAsset @unitnumber = '" + UNITNUMBER + "'")
         return assets;
     }
     catch(error) {
@@ -30,21 +25,9 @@ const viewAsset = async(UNITNUMBER) => {
     }
 }
 
-const initializeEquipment = async() => {
-    try {
-        let pool = await sql.connect(config);
-        pool.request().query("UPDATE dbo.Equipment SET STATUS = CASE WHEN dbo.Equipment.STATUS IS NULL THEN 1 ELSE dbo.Equipment.STATUS END")
-    }
-    catch(error) {
-        console.log(error)
-    }
-}
-
 const sendInService = async(USER, UNITNUMBER) => {
     try {
         let pool = await sql.connect(config);
-        pool.request().query("UPDATE Equipment SET STATUS = 1 WHERE UNITNUMBER = '" + UNITNUMBER + "'")
-        updateNotes(UNITNUMBER, null)
         logRequest(USER, UNITNUMBER, true);
     }
     catch(error) {
@@ -55,41 +38,19 @@ const sendInService = async(USER, UNITNUMBER) => {
 const sendOutOfService = async(USER, UNITNUMBER, NOTES) => {
     try {
         let pool = await sql.connect(config);
-        pool.request().query("UPDATE Equipment SET STATUS = 0 WHERE UNITNUMBER = '" + UNITNUMBER + "'")
-        updateNotes(UNITNUMBER, NOTES);
         logRequest(USER, UNITNUMBER, false);
+        pool.request().query("INSERT INTO Note(NOTE_ID, UNITNUMBER, NOTES) VALUES (NEWID(), '" + UNITNUMBER + "', '" + NOTES + "')");
     }
     catch(error) {
         console.log(error)
     }
 }
 
-const logRequest = async(USER, UNITNUMBER, movedInService) => {
+const logRequest = async(USER, UNITNUMBER, newStatus) => {
     try {
         let pool = await sql.connect(config);
-        let queryString = null
-        if(movedInService) 
-            queryString = "INSERT INTO Request(REQUEST_ID, [USER], UNITNUMBER, MOVED_IN_SERVICE, MOVED_OUT_OF_SERVICE) VALUES (NEWID(), '" + USER + "', '" + UNITNUMBER + "', '1', '0')";
-        else
-            queryString = "INSERT INTO Request(REQUEST_ID, [USER], UNITNUMBER, MOVED_IN_SERVICE, MOVED_OUT_OF_SERVICE) VALUES (NEWID(), '" + USER + "', '" + UNITNUMBER + "', '0', '1')";
-       
-        pool.request().query(queryString);
-    }
-    catch(error) {
-        console.log(error)
-    }
-}
-
-const updateNotes = async(UNITNUMBER, NOTES) => {
-    try {
-        let pool = await sql.connect(config);
-        let queryString = null
-        if(NOTES != null) 
-            queryString = "EXECUTE procUpdateInsertNotes @unitnumber = '" + UNITNUMBER + "', @notes = '" + NOTES + "'"
-        else
-            queryString = "EXECUTE procUpdateInsertNotes @unitnumber = '" + UNITNUMBER + "', @notes = NULL"
-       
-        pool.request().query(queryString);
+        let newStatusBit = newStatus ? '1' : '0';
+        pool.request().query("INSERT INTO Request(REQUEST_ID, [USER], UNITNUMBER, STATUS) VALUES (NEWID(), '" + USER + "', '" + UNITNUMBER + "', " + statusBit + ")");
     }
     catch(error) {
         console.log(error)
--- server.js
+++ server.js
@@ -2,9 +2,9 @@ const express = require('express'),
 db_operations = require('./db-utils/operations');
 
 
-db_operations.viewAll().then(res => {
-    console.log(res);
-})
+// db_operations.viewAll().then(res => {
+//     console.log(res);
+// })
 
 // db_operations.sendInService('ESanders', 'S211')
 // db_operations.sendInService('JFlores', 'S212')
@@ -14,6 +14,6 @@ db_operations.viewAll().then(res => {
 // db_operations.sendOutOfService('MBolding', 'S212', 'Radiator out.')
 // db_operations.sendOutOfService('ZNiece', 'S214', 'Bad fuel.')
 
-// db_operations.viewAsset('S211').then(res => {
-//     console.log(res)
-// })
+db_operations.viewAsset('S211').then(res => {
+    console.log(res)
+})
