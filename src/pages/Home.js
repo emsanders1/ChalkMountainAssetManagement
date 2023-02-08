dffd634f6eebe40fa265f59151b4c4fd68599114
Emma Sanders <emsanders@Mac-mini.lan>
2023-02-07 21:13:19 -0600

    table updates with mui

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import Table from "../components/Table"
+import SortableTable from "../components/Table"
 import Navbar from "../components/Navbar"
 import Switchbar from "../components/Switchbar"
 export default function Home(){
@@ -6,7 +6,7 @@ export default function Home(){
         <div className="Home">
         <Navbar/>
         <Switchbar/>
-        <Table/>
+        <SortableTable/>
         </div>
     )
 }
