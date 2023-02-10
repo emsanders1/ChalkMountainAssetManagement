a76785c993f05bcaf04536db43f677f166e2f1e6
Matthew Bolding <matthew.bolding@tcu.edu>
2023-02-10 12:27:00 -0600

    testing connections

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,14 @@
 import SortableTable from "../components/Table"
 import Navbar from "../components/Navbar"
 import Switchbar from "../components/Switchbar"
+import App from "../components/App"
 export default function Home(){
     return(
         <div className="Home">
         <Navbar/>
         <Switchbar/>
         <SortableTable/>
+        <App/>
         </div>
     )
 }
