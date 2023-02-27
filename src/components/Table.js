30d39bf4ad6c7af5a6358726c0a47daed27feef0
Emma Sanders <emsanders@Emmas-MacBook-Air-8.local>
2023-02-27 12:33:20 -0600

    updates

--- src/components/Table.js
+++ src/components/Table.js
@@ -18,34 +18,6 @@ import InputBase from "@mui/material/InputBase";
 import {useState} from "react";
 import SearchIcon from "@mui/icons-material/Search";
 
-function createData(type, asset, location, status, date, employee, notes, modify) {
-    return {
-        type,
-        asset,
-        location,
-        status,
-        date,
-        employee,
-        notes,
-        modify
-    };
-}
-
-// const rows = [
-//     createData('Trailer', 'S211', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", 'Tire Burst', <button>Update</button>),
-//     createData('Trailer', 'S212', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", 'Radiator Broken', <button>Update</button>),
-//     createData('Tractor', 'S214', 'ZT', 'IN-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S215', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S216', 'ZT', 'IN-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S217', 'ZT', 'IN-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Tractor', 'S218', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S220', 'ZT', 'IN-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S223', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Tractor', 'S234', 'ZT', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S245', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S256', 'ZT', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-//     createData('Trailer', 'S258', 'Pleasanton', 'OUT-OF-SERVICE', '12/05/22', "M.F.", '', <button>Update</button>),
-// ];
 
 const useStyles = makeStyles({
   root: {
@@ -72,6 +44,7 @@ function getComparator(order, orderBy) {
         ? (a, b) => descendingComparator(a, b, orderBy)
         : (a, b) => -descendingComparator(a, b, orderBy);
 }
+
 function stableSort(array, comparator) {
     const stabilizedThis = array.map((el, index) => [el, index]);
     stabilizedThis.sort((a, b) => {
@@ -302,89 +275,3 @@ export default function EnhancedTable() {
     );
 }
 
-// import React, { useState, useEffect } from 'react';
-// import { makeStyles } from '@material-ui/core/styles';
-// import Table from '@material-ui/core/Table';
-// import TableBody from '@material-ui/core/TableBody';
-// import TableCell from '@material-ui/core/TableCell';
-// import TableHead from '@material-ui/core/TableHead';
-// import TableRow from '@material-ui/core/TableRow';
-// import Paper from '@material-ui/core/Paper';
-
-// const useStyles = makeStyles({
-//   root: {
-//     width: '100%',
-//     overflowX: 'auto',
-//   },
-//   table: {
-//     minWidth: 650,
-//   },
-// });
-
-
-// function descendingComparator(a, b, orderBy) {
-//         if (b[orderBy] < a[orderBy]) {
-//             return -1;
-//         }
-//         if (b[orderBy] > a[orderBy]) {
-//             return 1;
-//         }
-//         return 0;
-// }
-    
-// function getComparator(order, orderBy) {
-//     return order === 'desc'
-//         ? (a, b) => descendingComparator(a, b, orderBy)
-//         : (a, b) => -descendingComparator(a, b, orderBy);
-// }
-// function stableSort(array, comparator) {
-//     const stabilizedThis = array.map((el, index) => [el, index]);
-//     stabilizedThis.sort((a, b) => {
-//         const order = comparator(a[0], b[0]);
-//         if (order !== 0) {
-//             return order;
-//         }
-//         return a[1] - b[1];
-//     });
-//     return stabilizedThis.map((el) => el[0]);
-// }
-
-// export default function DenseTable() {
-//   const classes = useStyles();
-//   const [data, setData] = useState([]);
-
-//   useEffect(() => {
-//     fetch('https://localhost:8090/api/assets')
-//       .then(response => response.json())
-//       .then(data => setData(data));
-//   }, []);
-
-//   return (
-//     <Paper className={classes.root}>
-//       <Table className={classes.table} size="small">
-//         <TableHead>
-//           <TableRow>
-//             <TableCell>Type</TableCell>
-//             <TableCell align="right">Asset</TableCell>
-//             <TableCell align="right">Location</TableCell>
-//             <TableCell align="right">Status</TableCell>
-//             <TableCell align="right">Date Last Modified</TableCell>
-//             <TableCell align="right">Employee</TableCell>
-//             <TableCell align="right">Notes</TableCell>
-//             <TableCell align="right">Modify</TableCell>
-//           </TableRow>
-//         </TableHead>
-//         <TableBody>
-//           {data.map(row => (
-//             <TableRow key={row.id}>
-//               <TableCell component="th" scope="row">{row.LOCATION}</TableCell>
-//               <TableCell align="right">{row.title}</TableCell>
-//               <TableCell align="right">{row.body}</TableCell>
-//             </TableRow>
-//             // change to unitnumber, location, status, most_recent_update, user, notes
-//           ))}
-//         </TableBody>
-//       </Table>
-//     </Paper>
-//   );
-// }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import SortableTable from "../components/Table"
 import Navbar from "../components/Navbar"
 import Switchbar from "../components/Switchbar"
-import App from "../components/App"
 export default function Home(){
     return(
         <div className="Home">
